e7bc71cd033ed0b1b410746d60cf599c22d1219a
David McCoy <david.mccoy@gmail.com>
2018-12-30 19:01:25 -0500

    added cors gem

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,7 +2,8 @@ class EpisodesController < ApplicationController
   def create
     Episode.where(
       blubrry_filename: params[:filename],
-      blubrry_file_url: params[:file_url]
+      blubrry_file_url: params[:file_url],
+      blubrry_date: params[:date]
     ).first_or_create
   end
 end
