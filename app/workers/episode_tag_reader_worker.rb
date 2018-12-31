409e4266ba272f3aafe78b0e97c5ab8c35e8750c
David McCoy <david.mccoy@gmail.com>
2018-12-31 12:59:13 -0500

    moved url open into a block to reduce memory and connection numbers

--- app/workers/episode_tag_reader_worker.rb
+++ app/workers/episode_tag_reader_worker.rb
@@ -5,12 +5,12 @@ class EpisodeTagReaderWorker
 
   def perform(episode_id)
     episode = Episode.find_by_id(episode_id)
-    episode_file = open(episode.blubrry_file_url) if episode
-    episode_tags = ID3Tag.read(episode_file) if episode_file
-
-    episode.update(
-      title: episode_tags.title,
-      artist: episode_tags.artist
-    )
+    episode_file = open(episode.blubrry_file_url) { |file|
+      episode_tags = ID3Tag.read(file)
+      episode.update(
+        title: episode_tags.title,
+        artist: episode_tags.artist
+      )
+    }
   end
 end
