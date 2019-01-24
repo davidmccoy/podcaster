e054e724a30bfd13c7e2c858b7dbdac2ce122c7e
David McCoy <david.mccoy@gmail.com>
2019-01-23 22:28:39 -0500

    removed aws config

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'us-east-1',
-  credentials: Aws::Credentials.new(Figaro.env.s3_key, Figaro.env.s3_secret),
-})
-
-S3_PODCASTS = Aws::S3::Resource.new.bucket(Figaro.env.s3_bucket)
