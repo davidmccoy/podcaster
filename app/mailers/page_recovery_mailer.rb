5daacefc9a607d6203d395119aa1755d7c2c74eb
David McCoy <david.mccoy@gmail.com>
2019-02-02 20:57:13 -0500

    recover email supports unlisted podcasts

--- app/mailers/page_recovery_mailer.rb
+++ app/mailers/page_recovery_mailer.rb
@@ -2,7 +2,7 @@ class PageRecoveryMailer < ApplicationMailer
   def new_recovery_request(name, email, podcast_name, description)
     @name = name
     @email = email
-    @podcast_name = podcast_name
+    @podcast_name = podcast_name.blank? ? 'an Unlisted Podcast' : podcast_name
     @podcast = Page.find_by_name(@podcast_name)
     @description = description
     @url =
@@ -12,6 +12,6 @@ class PageRecoveryMailer < ApplicationMailer
         nil
       end
 
-    mail(to: 'david@hipstersofthecoast.com', subject: "A new recovery request has been made for #{podcast_name}.")
+    mail(to: 'david@hipstersofthecoast.com', subject: "A new recovery request has been made for #{@podcast_name}.")
   end
 end
