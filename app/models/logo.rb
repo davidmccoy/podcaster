a6e7f1e3a6a5f8781663b425bd286671df0fe190
David McCoy <david.mccoy@gmail.com>
2019-09-12 19:13:29 -0500

    bugfix?

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,5 +1,6 @@
 class Logo < Image
   def url(size)
+    p file
     if file_attacher.stored? && file
       "https://#{ENV["IMAGES_S3_BUCKET"]}.s3.amazonaws.com/#{fetch_image(size).id}"
     elsif file_attacher.cached? && file
