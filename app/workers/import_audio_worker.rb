9ba0fa909e0a82e08f097e00140d7a9effd6059c
David McCoy <david.mccoy@gmail.com>
2020-09-17 14:54:44 -0400

    support .m4a

--- app/workers/import_audio_worker.rb
+++ app/workers/import_audio_worker.rb
@@ -4,12 +4,13 @@ class ImportAudioWorker
 
   def perform(podcast_episode_id, url)
     podcast_episode = PodcastEpisode.find(podcast_episode_id)
+    file_location = ::RedirectFollower.new(url).resolve.url
 
     Audio.new.tap do |a|
       a.attachable_type = 'PodcastEpisode'
       a.attachable_id = podcast_episode_id
       a.label = 'podcast_episode'
-      a.file_remote_url = ::RedirectFollower.new(url).resolve.url
+      a.file_remote_url = file_location
       a.save!
     end
   rescue => e
