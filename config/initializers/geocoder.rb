d7325cc599b4f408fd493a76dbe92c3722bd1c13
David McCoy <david@davids-mbp.myfiosgateway.com>
2023-02-11 22:18:32 -0500

    correct redis connection for geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,5 @@
 Geocoder.configure(
   ip_lookup: :ipinfo_io,
   api_key: ENV['APINFO_API_KEY'],
-  cache: REDIS,
+  cache: Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }),
 )
